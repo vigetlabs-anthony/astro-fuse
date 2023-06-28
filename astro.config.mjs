853175f23f32733b51eef6f52244718de90d94e8
Anthony <anthony.ngo@viget.com>
2023-06-27 23:50:15 -0700

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "static",
+  adapter: vercelStatic({
+    analytics: true,
+  }),
 });
